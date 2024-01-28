530628f3d69f2444495028e28b8f7a2af4a4ca2e
edgsel <edgar.selihov@gmail.com>
2024-01-28 18:51:14 +0200

    WWA-000 - remove unused request logging config, replace deprecated webfliter config with up to date config

--- weather-api-client/src/main/kotlin/usr/edgsel/weatherapiclient/config/WebSecurityConfig.kt
+++ weather-api-client/src/main/kotlin/usr/edgsel/weatherapiclient/config/WebSecurityConfig.kt
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -16,28 +13,10 @@ class WebSecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
-            .headers()
-            .frameOptions().sameOrigin()
-            .and()
-            .authorizeRequests()
-            .requestMatchers("/websocket/**", "/app/**").permitAll() // Permit access to WebSocket endpoints
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic()
-            .and()
-            .csrf().disable().build() // Disable CSRF for WebSocket
-    }
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val config = CorsConfiguration().also {
-            it.allowedOrigins = listOf("*")
-            it.allowCredentials = true
-            it.allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
-            it.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
-        }
-
-        return UrlBasedCorsConfigurationSource()
-            .apply { registerCorsConfiguration("/**", config) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/websocket/**", "/app/**").permitAll()
+            }
+            .csrf { it.disable() }
+            .build()
     }
 }
